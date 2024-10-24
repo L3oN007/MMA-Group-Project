253121edd8f936c9ca3b193b6d73d8b04742b60b
Gia Khang <khanggia85@gmail.com>
2024-10-25 01:19:43 +0700

    added role checker

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,12 +1,41 @@
+import { useUsers } from "@/hooks/userUsers";
+import useAuthStore from "@/stores/useAuthStore";
+import { UserRole } from "@/types/user.type";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 
 const Layout = () => {
+  const { mutateAsync: getUserInfo, isPending: isGetting } = useUsers();
+  const { user } = useAuthStore();
+
+  useEffect(() => {
+    // Init user info
+    getUserInfo();
+  }, []);
+
+  const onGetTabByRole = (role: string | undefined) => {
+    switch (role) {
+      case UserRole.STAFF:
+      case UserRole.MANAGER:
+      case UserRole.ADMIN:
+        return tabs[role.toLocaleLowerCase() as keyof typeof tabs];
+      default:
+        return tabs.user;
+    }
+  }
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name={`(adminTabs)`} options={{ headerShown: false }} />
       <Stack.Screen name="blog/[blogId]" options={{ headerShown: false }} />
     </Stack>
   );
 };
+
+const tabs = {
+  user: 'userTabs',
+  staff: 'staffTabs',
+  manager: 'managerTabs',
+  admin: 'adminTabs',
+}
 
 export default Layout;
